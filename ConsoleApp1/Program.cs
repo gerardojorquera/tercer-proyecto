c04c39247aac6e14cf2f2e8845414925a5017de6
Gerardo Jorquera <gjorquera@medicenter.cl>
2021-02-13 10:02:41 -0400

    ohhh

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Prueba git");
+            Console.WriteLine("ohhhhh");
             Console.ReadKey();
         }
     }
